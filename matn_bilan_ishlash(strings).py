dacab336b98bdc277a3e21ca8b11473b32160b36
husniddin-sheyx <152167269+husniddin-sheyx@users.noreply.github.com>
2024-02-02 22:06:14 +0500

    Create matn_bilan_ishlash(strings).py

---
+++ matn_bilan_ishlash(strings).py
@@ -0,0 +1,33 @@
+#ism = 'Husnidddin'
+#familya = 'Sadriddinov'
+#print(ism + ' ' + familya)
+
+#ism = 'Husniddin'
+#familya = 'Sadriddinov'
+#ism_sharif = f"{ism} {familya}" # f string bilan ishlash
+#print(ism_sharif)
+
+#ism = 'Husniddin'
+#familya = 'Sadriddinov'
+#print(f"Salom, mening ismim {familya}. {ism} {familya}" )
+
+#\t uzun boshliq qoldiradi
+#print("hello\tworld") 
+
+# STRING METODLAR
+
+#ism = 'james'
+#familiya = 'bont'
+#ism_sharif =f"{ism} {familiya}"
+#ism_sharif = ism_sharif.upper()
+#print(ism_sharif.lower())    
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+
+#meva = "  olma  "
+#print("men " + meva.lstrip() + "ni yaxshi ko'raman")
+#print("men " + meva.rstrip() + "ni yaxshi ko'raman")
+#print("men " + meva.strip() + "ni yaxshi ko'raman")
+
+
+
