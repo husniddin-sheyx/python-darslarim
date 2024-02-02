ceb0ffed7f477c4680c2f2d8fdeb553dc670e76e
husniddin-sheyx <152167269+husniddin-sheyx@users.noreply.github.com>
2024-02-02 22:06:18 +0500

    Create lists_ro'yxatlar.py

---
+++ lists_ro'yxatlar.py
@@ -0,0 +1,36 @@
+# cars = []
+# cars.append('tico')
+# cars.append('tracer')
+# cars.append("nexia")
+# cars.append('lasetti')
+# cars.remove("tracer")
+# print(cars)
+# del cars[5] # bu sonlarni ochirish uchun
+
+
+
+# mashinalar = ['bmw', 'mersades', 'supra', 'honda']
+# mashinalar.insert(2, "MATIZ")
+# print(mashinalar)
+
+# print(mashinalar[2]())
+# print(mashinalar[0].upper())
+
+# mashinalar[2] = 'nexia 3'
+# mashinalar[-3] = 'cobalt'
+
+# mashinalar = ['bmw', 'mersades', 'supra', 'honda', 'nexia' ]
+narxlar = [800000, 20000, 309000, 430000,42300 -32 2.1]
+# sonlar = ['bir', 'ikki', 3,4,5]
+# ismlar = []   
+
+# mashinalar[-1] = 'damas'
+# print(mashinalar)
+
+# cars = ['bmw', 'audi', 'nissan', 'supra', 'benz', 'volva']
+# input(len(cars))
+# print(cars)
+
+# narxlar [0] + narxlar [1]        
+ 
+
