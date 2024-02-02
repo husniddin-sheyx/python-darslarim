eb196fc97676bef96becdc24087876ce4a8967ac
husniddin-sheyx <152167269+husniddin-sheyx@users.noreply.github.com>
2024-02-02 22:06:05 +0500

    Create Sonlar.py

---
+++ Sonlar.py
@@ -0,0 +1,21 @@
+# a = 20 
+# b = 3.4
+# temp = 36.6
+# print(type())
+
+# aholi_soni = 7_232_456_345
+# print(aholi_soni)
+
+# radius = 20
+# PI = 3.14159
+# diametr = 2*radius
+# print("aylana uzuznligi=", PI*diametr)
+
+# ism = "Husniddin"
+# yosh = '19'
+# xabar = ism + ' ' + yosh + ' yoshda'
+# print(xabar)
+
+# t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# yosh = 2023 - t_yil
+# print("Siz",yosh, "yoshda ekansiz")
