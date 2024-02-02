c43d8b0f483416f3cf5299e71569a789f22fd234
husniddin-sheyx <152167269+husniddin-sheyx@users.noreply.github.com>
2024-02-02 22:05:42 +0500

    Create print_ishlash.py

---
+++ print_ishlash.py
@@ -0,0 +1,25 @@
+# print ("hello world")
+
+#ism = "Husniddin"
+
+#shahar = "uychi"
+#viloyat = "namangan"
+
+#print("husnidddin \"uychida\" yashaydi")
+
+#print("""nimani eksang,
+#shuni orasan""")
+
+#print("harakat,\nnatijaga bog'liq")
+
+# print ('Odami ersang,demagilodami,\noniki,yo\'q xalq g\'amidin g\'am')
+
+#ARIFMETIK AMALLAR 
+#print(2+8*5)
+#print(34/2) #BUNDA BUTUN SON KO'RINISHIDA CHIQMAYDI
+#print(34//2) #BUNDA BUTUN SON KO'RINISHDA CHIQADI
+#print(2**5) #BUNDA SONLARNI KVADRADGA KO'TARADI
+
+#print("TO'QQIZNING KVADRATI",9**2,"GA TENG")
+#print('3*3=',3*3)
+  
